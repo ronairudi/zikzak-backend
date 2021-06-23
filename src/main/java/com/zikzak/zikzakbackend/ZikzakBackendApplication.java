1fb0ef0586760cc66b39629dc8f65195d335deb4
ronairudi <ronai.rudi98@gmail.com>
2021-06-23 09:32:34 +0200

    feat: initialize spring boot project

---
+++ src/main/java/com/zikzak/zikzakbackend/ZikzakBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zikzak.zikzakbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZikzakBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZikzakBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zikzak/zikzakbackend/ZikzakBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zikzak.zikzakbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZikzakBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
