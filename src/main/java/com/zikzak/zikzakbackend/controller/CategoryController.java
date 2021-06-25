0a5b1d57f586d867ae841c901573f4da398dbf3f
Kriszti Toth <k95toth@gmail.com>
2021-06-25 14:47:30 +0200

    feat: set allowed origins

--- src/main/java/com/zikzak/zikzakbackend/controller/CategoryController.java
+++ src/main/java/com/zikzak/zikzakbackend/controller/CategoryController.java
@@ -4,10 +4,12 @@ import com.zikzak.zikzakbackend.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@CrossOrigin(origins = {"${development.url}", "${production.url}"})
 @RequestMapping("/categories")
 public class CategoryController {
 
