4564d0e49cdec976dc7f192da0b7008fa756fa2f
Kriszti Toth <k95toth@gmail.com>
2021-06-30 16:06:26 +0200

    feat: create jwt token services

---
+++ src/main/java/com/zikzak/zikzakbackend/security/JwtTokenServices.java
@@ -0,0 +1,38 @@
+package com.zikzak.zikzakbackend.security;
+
+import com.zikzak.zikzakbackend.model.Role;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtTokenServices {
+
+    private String secretKey = "e455bfa8-7c7f-4b17-8bcd-aebffe258c6c";
+    private long validityInMilliseconds = 3600000;
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    public String createToken(String username, Role role) {
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put("role", role);
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+}
