733774a5061024eb30993fbd8c21e2bc37af2583
Solya Gergo <solya.gergo.sandor@gmail.com>
2021-07-07 15:45:07 +0200

    fix: password now cannot be read

--- src/main/java/com/zikzak/zikzakbackend/model/UserModel.java
+++ src/main/java/com/zikzak/zikzakbackend/model/UserModel.java
@@ -1,5 +1,6 @@
 package com.zikzak.zikzakbackend.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -23,6 +24,7 @@ public class UserModel {
     private String email;
 
     @NotEmpty
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
     @Enumerated(EnumType.STRING)
